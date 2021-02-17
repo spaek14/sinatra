8cff22da6261ab97dff8e6d51c21cbeaaec1531a
spaek14 <>
2021-02-17 15:38:39 +0000

    my cat

--- app.rb
+++ app.rb
@@ -4,8 +4,13 @@ get '/' do
   'Hello World! how are you'
 end
 
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='https://i1.wp.com/katzenworld.co.uk/wp-content/uploads/2019/06/funny-cat.jpeg?w=1920&ssl=1'>
-   </div>"
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
+end
+
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb :index
 end
